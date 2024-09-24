414209b41eb7d86a26454275717e2fb478fbca7c
Namibo06 <namibo654@gmail.com>
2024-09-24 16:20:45 -0300

    create project

---
+++ app/src/main/java/br/com/barbx/HomeActivity.java
@@ -0,0 +1,12 @@
+package br.com.barbx;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
