f6b55a9dc346bac4b8f9ccbecc96474794ca4ff7
Namibo06 <namibo654@gmail.com>
2024-09-26 01:48:56 -0300

    feat: added glide config for image view

--- app/src/main/java/br/com/barbx/HomeActivity.java
+++ app/src/main/java/br/com/barbx/HomeActivity.java
@@ -1,12 +1,24 @@
 package br.com.barbx;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+
 public class HomeActivity extends AppCompatActivity {
+    @SuppressLint("ResourceType")
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ImageView imageView = (ImageView) findViewById(R.id.image_barber_background);
+
+        Glide.with(this)
+                .load(R.drawable.barber_bg)
+                .into(imageView);
     }
 }
