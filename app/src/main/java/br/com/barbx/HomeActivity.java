e30f0d275a87abf0fd70636961614e9f3fb61b02
Namibo06 <namibo654@gmail.com>
2024-10-01 16:45:35 -0300

    feat: create new activities

--- app/src/main/java/br/com/barbx/HomeActivity.java
+++ app/src/main/java/br/com/barbx/HomeActivity.java
@@ -2,6 +2,7 @@ package br.com.barbx;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +15,18 @@ public class HomeActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // Altera a cor da barra de navegação para transparente
+        getWindow().setNavigationBarColor(android.graphics.Color.TRANSPARENT);
+        // Se você também quiser que a barra de status fique transparente
+        getWindow().setStatusBarColor(android.graphics.Color.TRANSPARENT);
+
+        // Para permitir que o conteúdo se estenda sob a barra de navegação
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
 
         ImageView imageView = (ImageView) findViewById(R.id.image_barber_background);
 
--- app/src/main/java/br/com/barbx/MainActivity.java
+++ app/src/main/java/br/com/barbx/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().setNavigationBarColor(android.graphics.Color.TRANSPARENT);
+        getWindow().setStatusBarColor(android.graphics.Color.TRANSPARENT);
+
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
 
         new Handler().postDelayed(new Runnable() {
 
@@ -21,6 +31,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        },3000);
+        },2000);
     }
 }
